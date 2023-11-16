2567be7cb305a4e013d81c685d5dcf0c519e44bd
awooooool <45804108+awooooool@users.noreply.github.com>
2023-11-16 22:46:06 +0700

    feat(main): send response as JSON

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from helper import imdsv2
 import uvicorn
 
@@ -6,10 +8,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {
+    response = jsonable_encoder({
         "instance-id": imdsv2.getInstanceID(),
         "local-hostname": imdsv2.getInstanceHostname(),
-    }
+    })
+    return JSONResponse(content=response)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
