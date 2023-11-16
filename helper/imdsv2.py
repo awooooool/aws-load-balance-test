6b00a4ac9664bb4d8ff3124cbd23d054558aa1c0
awooooool <45804108+awooooool@users.noreply.github.com>
2023-11-16 23:00:33 +0700

    docs: annotate functions & variables

--- helper/imdsv2.py
+++ helper/imdsv2.py
@@ -1,11 +1,14 @@
 import http.client
 
+# IMDSv2 Address
 IMDS_ADDRESS = "169.254.169.254"
 
+# Header for requesting token
 AWS_REQ_TOKEN_HEADER = {
     "X-aws-ec2-metadata-token-ttl-seconds": 21600,
 }
 
+# To keep myself sane and not repeating this
 def req(method, address, path, headers):
     conn = http.client.HTTPConnection(address)
     conn.request(method, path, headers=headers)
@@ -13,16 +16,21 @@ def req(method, address, path, headers):
     body = res.read()
     return body
 
+# Get token, otherwise IMDS will spit 401 Unauthorized
+# For more details, see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
 def getToken():
     return req("PUT", IMDS_ADDRESS, "/latest/api/token", AWS_REQ_TOKEN_HEADER)
 
+# Header for subsequent requests
 def assembleReqHeader():
     return {
         "X-aws-ec2-metadata-token": getToken()
     }
 
+# Request instance ID from IMDS
 def getInstanceID():
     return req("GET", IMDS_ADDRESS, "/latest/meta-data/instance-id", assembleReqHeader())
 
+# Request instance hostname from IMDS
 def getInstanceHostname():
     return req("GET", IMDS_ADDRESS, "/latest/meta-data/local-hostname", assembleReqHeader())
--- main.py
+++ main.py
@@ -8,11 +8,14 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    # Encode response into JSON
     response = jsonable_encoder({
-        "instance-id": imdsv2.getInstanceID(),
-        "local-hostname": imdsv2.getInstanceHostname(),
+        "instance-id": imdsv2.getInstanceID(), # Get instance ID from IMDSv2
+        "local-hostname": imdsv2.getInstanceHostname(), # Get instance local hostname from IMDSv2
     })
+    # Send the response
     return JSONResponse(content=response)
 
+# Debug only
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
